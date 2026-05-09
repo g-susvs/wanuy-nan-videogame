26123707dfad11cc0556d427eb3125e14caa552e
Jesus Valencia <jesus.savs19@gmail.com>
2026-05-09 18:15:52 -0500

    feat: 🎸 add tileset, map and basic player.tscn and player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,30 @@
 extends CharacterBody2D
 
+@export var speed: float = 220.0
+@export var jump_force: float = -420.0
+@export var gravity: float = 900.0
+@export var max_lives: int = 5
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var lives: int
+var is_dead: bool = false
 
+func _ready() -> void:
+	lives = max_lives
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	if is_dead:
+		return
+	
+	# Aplicar gravedad
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		velocity.y += gravity * delta
+	
+	# Movimiento horizontal
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity.x = direction * speed
+	
+	# Salto
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_force
+	
 	move_and_slide()
