e71811518b7dd8eeaba8f80ed836f5ff6e7bab30
Jesus Valencia <jesus.savs19@gmail.com>
2026-05-10 00:23:01 -0500

    feat: 🎸 add enemy patrol

--- scripts/melee_enemy.gd
+++ scripts/melee_enemy.gd
@@ -3,12 +3,14 @@ extends CharacterBody2D
 @export var speed: float = 120.0
 @export var gravity: float = 900.0
 @export var stop_distance: float = 40.0
+
 @onready var attack_area = $AttackArea
 @onready var attack_cooldown = $AttackCooldown
+@onready var patrol_timer = $PatrolTimer
 
 var can_attack := true
 var player_in_attack_range := false
-
+var patrol_direction := 1
 
 var player = null
 
@@ -18,26 +20,30 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
+	# SI NO HAY JUGADOR → patrulla
 	if player == null or player.is_dead:
-		velocity.x = 0
-		move_and_slide()
-		return
 		
-	var distance = player.global_position.x - global_position.x
-		
-	# Seguir solo si está lejos
-	if abs(distance) > stop_distance:
-		
-		var direction = sign(distance)
-		velocity.x = direction * speed
+		velocity.x = patrol_direction * speed
+	
 	else:
-		velocity.x = 0
+		
+		var distance = player.global_position.x - global_position.x
+		
+		# Seguir jugador
+		if abs(distance) > stop_distance:
+			
+			var direction = sign(distance)
+			velocity.x = direction * speed
+		
+		else:
+			velocity.x = 0
+		
+		# Atacar
+		if player_in_attack_range:
+			attack()
 	
 	move_and_slide()
 	
-	if player_in_attack_range:
-		attack()
-
 func attack():
 	if can_attack == false:
 		return
@@ -71,3 +77,7 @@ func _on_attack_area_body_exited(body):
 
 func _on_attack_cooldown_timeout():
 	can_attack = true
+
+
+func _on_patrol_timer_timeout() -> void:
+	patrol_direction *= -1
